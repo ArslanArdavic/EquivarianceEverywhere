910a701f9b7434f9a228091c7b204d7c7e5e5bbd
Arda Arslan <arda.arslan@boun.edu.tr>
2025-09-24 14:21:12 +0300

    API Token inserted

--- helpers/constants.py
+++ helpers/constants.py
@@ -6,4 +6,4 @@ DECIMAL = 2
 SEEDS = list(range(5))
 TASK_LOSS = CrossEntropyLoss()
 
-API_TOKEN = "..."  # insert your token to use neptune
+API_TOKEN = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJjYjlhZjMxMS1mZjgyLTQ4Y2YtYmY5ZC1mMjVjOWU2YmI4YWMifQ=="  # insert your token to use neptune
