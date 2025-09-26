418ecc54cd54b1a14c0324d0157d0704faeda117
Arda Arslan <arda.arslan@boun.edu.tr>
2025-09-26 11:45:27 +0300

    log file name changed

--- run-job.sh
+++ run-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=tsgnn-train
-#SBATCH --output=logs/tsgnn-train_%j.out
-#SBATCH --error=logs/tsgnn-train_%j.err
+#SBATCH --output=logs/tsgnn-test_%j.out
+#SBATCH --error=logs/tsgnn-test_%j.err
 #SBATCH --time=24:00:00
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
